366deaddc76f95bab11c3813f347e00587d91eeb
rakesh <kallurirakesh15@gmail.com>
2020-10-21 11:27:48 -0400

    added basic state to app

--- src/index.js
+++ src/index.js
@@ -13,16 +13,32 @@ import ReactDom from 'react-dom';
 //   return <div>Hi there</div>;
 // };
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, errorMsg: '' };
     window.navigator.geolocation.getCurrentPosition(
-      (success) => {
-        console.log(success);
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
       },
       (error) => {
-        console.log(error);
+        this.setState({ errorMsg: error.message });
       }
     );
-    return <div>Latitude:</div>;
+  }
+  componentDidMount() {
+    console.log('component did mount called');
+  }
+  componentDidUpdate() {
+    console.log('component did update called');
+  }
+  render() {
+    if (this.state.errorMsg && !this.state.lat) {
+      return <div>Error:{this.state.errorMsg}</div>;
+    }
+    if (!this.state.errorMsg && this.state.lat) {
+      return <div>Latitude:{this.state.lat}</div>;
+    }
+    return <div>Loading..</div>;
   }
 }
 ReactDom.render(<App />, document.querySelector('#root'));
